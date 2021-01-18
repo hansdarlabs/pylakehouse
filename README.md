ee9d0fe25a1932627bcc0f17720b1fa11cf7c275
Senthil Nayagan <senthil.nayagan@gmail.com>
2021-01-18 09:30:35 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # lakehouse
 lakehouse platform keeps delta data in various zones (Bronze, Silver, etc.) and expose them via REST API endpoints.
+
+## Prerequisite
+- Python 3.8.x (64-bit)
+- Refer requirements.pip for dependent Python libraries
+
+## Contributors
+- Hansdar Team
+
+## License
+The project is licensed under the Apache 2 license
--- com/hansdar/lakehouse/util/sparkutil.py
+++ com/hansdar/lakehouse/util/sparkutil.py
@@ -1,28 +1,17 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import lit
-from pyspark.sql.types import StringType
 from logutil import *
 
 
 def create_spark_session():
     """Creates Spark session."""
     spark_session = SparkSession.builder.master("local[*]")\
-        .appName("ScrumAssistant")\
+        .appName("lakehouse")\
         .config("spark.executor.memory", "3g")\
         .config("spark.executor.instances", 1)\
         .config("spark.executor.cores", 4)\
         .config("spark.driver.memory", "2g")\
         .config("spark.files.maxPartitionBytes", 33554432)\
-        .enableHiveSupport()\
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .getOrCreate()
-    spark_session.conf.set("spark.sql.shuffle.partitions", 24)
     return spark_session
-
-
-def cast_to_string(all_columns):
-    """Casts all columns to String."""
-    string_columns = []
-
-    for each_column in all_columns:
-        string_columns.append(lit(None).cast(StringType()).alias('{0}'.format(each_column)))
-    return string_columns
